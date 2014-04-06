cbf448436b9836430517611a50f179dd213ec7ef
Li <vjzkbb@gmail.com>
2014-04-06 16:26:38 +0400

    index

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -10,9 +10,58 @@
 </head>
 
 <body>
-
-<?php echo $content; ?>
-
+<div class="wrapper">
+    <div class="aside">
+        <div class="aside-content">
+            <div class="aside-header">
+                <div class="city">
+                    <span class="city-name">Казань</span>
+                    <span class="change-city">Выбрать другой город</span>
+                </div>
+                <div class="phone">+7 (843) 567-98-01</div>
+                <div class="callback">Заказать обратный звонок</div>
+            </div>
+            <div class="aside-menu">
+                <div class="logo">
+                    <div class="logo-p">PENTA</div>
+                    <div class="logo-h">HOUSE</div>
+                    <div class="logo-description">Элитная сантехника, плитка.<br> Продажа. Монтаж. Сервис.</div>
+                </div>
+                <ul class="menu">
+                    <li class="active">
+                        <a href="#">О компании</a>
+                    </li>
+                    <li>
+                        <a href="#">Дилерство</a>
+                    </li>
+                    <li>
+                        <a href="#">Каталог</a>
+                    </li>
+                    <li>
+                        <a href="#">Проекты</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="aside-footer">
+            <div class="aside-footer-wrapper">
+                <div class="social">
+                    <a href="#" class="vk"></a>
+                    <a href="#" class="fc"></a>
+                    <a href="#" class="tw"></a>
+                </div>
+                <div class="copyright">&copy; 2013 Penta House</div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <?php echo $content; ?>
+        <div class="anchor"></div>
+    </div>
+</div>
 </body>
 </html>
 
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,96 +1,132 @@
 <?php
-/* @var $this SiteController */
-
-$this->pageTitle="Penta House - Элитная сантехника и плитка. Продажа. Монтаж. Сервис.";
+$this->pageTitle = "Penta House - Элитная сантехника и плитка. Продажа. Монтаж. Сервис.";
 ?>
 
 
-<table class="content">
-    <tr>
-        <td class="first-col" rowspan="8"></td>
-        <td class="banner" colspan="4" rowspan="3"></td>
-        <td class="brand-col last-col" style="background: url(/images/villeroy_boch.jpg) center center">
-            <img src="/images/vb-logo.png" />
-        </td>
-    </tr>
-    <tr>
-        <td class="brand-col last-col" style="background: url(/images/dornbracht_1.jpg) center center">
-            <img src="/images/db-logo.png" />
-        </td>
-    </tr>
-    <tr>
-        <td class="brand-col last-col" style="background: url(/images/al.jpg) center center">
-            <img src="/images/al-logo.png" />
-        </td>
-    </tr>
-    <tr>
-        <td class="service">
-            <h1>Дизайн и&nbsp;проектирование</h1>
-            <div class="service-line"></div>
-            <div class="service-description">Бесплатный замер<br/>и&nbsp;дизайн-проект</div>
-        </td>
-        <td class="service">
-            <h1>Продажа</h1>
-            <div class="service-line"></div>
-            <div class="service-description">Элитная сантехника<br/>по&nbsp;доступным ценам</div>
-        </td>
-        <td class="service">
-            <h1>Установка и&nbsp;монтаж</h1>
-            <div class="service-line"></div>
-            <div class="service-description">Бесплатная установка и&nbsp;монтаж&nbsp;с&nbsp;гарантией</div>
-        </td>
-        <td class="service">
-            <h1>Сервисное обслуживание</h1>
-            <div class="service-line"></div>
-            <div class="service-description">Гарантийное обслуживание сантехники</div>
-        </td>
-        <td class="brand-col last-col" style="background: url(/images/hg.jpg) center center">
-            <img src="/images/hg-logo.png" />
-        </td>
-    </tr>
-    <tr>
-        <td class="headline">
-            Проекты <span class="quantity">4</span>
-        </td>
-        <td style="background: url(/images/bath1.jpg) center center"></td>
-        <td style="background: url(/images/bath2.jpg) center center"></td>
-        <td style="background: url(/images/bath4.jpg) center center"></td>
-        <td class="brand-col last-col" style="background: url(/images/gessi-bg.jpg) center center">
-            <img src="/images/gessi-logo.png" />
-        </td>
-    </tr>
-    <!--
-    <tr>
-        <td style="background: url(bath5.jpg) center center"></td>
-        <td style="background: url(bath6.jpg) center center"></td>
-        <td style="background: url(bath7.jpg) center center"></td>
-        <td style="background: url(bath8.jpg) center center"></td>
-        <td class="brand-col last-col" style="background: url(duravit-bg.jpg) center center">
-            <img src="duravit-logo.png" />
-        </td>
-    </tr>
-    -->
-    <tr>
-        <td style="background: url(/images/news1.jpg) center center"></td>
-        <td class="headline">
-            Новости <span class="quantity">9</span>
-        </td>
-        <td style="background: url(/images/news2.jpg) center center"></td>
-        <td style="background: url(/images/news3.jpg) center center"></td>
-        <td class="brand-col last-col" style="background: url(/images/keuco-bg.jpg) center center">
-            <img src="/images/keuco-logo.png" />
-        </td>
-    </tr>
-    <!--
-    <tr>
-        <td style="background: url(news4.jpg) center center"></td>
-        <td style="background: url(news5.jpg) center center"></td>
-        <td style="background: url(news6.jpg) center center"></td>
-        <td style="background: url(news7.jpg) center center"></td>
-        <td class="brand-col last-col" style="background: url(jacuzzi-bg.jpg) center center">
-            <img src="jacuzzi-logo.png" />
-        </td>
-    </tr>
-    -->
-</table>
+<div class="index-box">
+    <div class="main-img"></div>
+    <div class="service">
+        <div class="service-item">
+            <div class="service-item-wrapper">
+                <h3>Дизайн и проектирование</h3>
+
+                <div class="service-hr"></div>
+                <div class="service-description">Бесплатный замер и дизайн-проект</div>
+            </div>
+        </div>
+        <div class="service-item">
+            <div class="service-item-wrapper">
+                <h3>Продажа</h3>
+
+                <div class="service-hr"></div>
+                <div class="service-description">Бесплатный замер и проект</div>
+            </div>
+        </div>
+        <div class="service-item">
+            <div class="service-item-wrapper">
+                <h3>Дизайн</h3>
+
+                <div class="service-hr"></div>
+                <div class="service-description">замер и дизайн-проект</div>
+            </div>
+        </div>
+        <div class="service-item">
+            <div class="service-item-wrapper">
+                <h3>Проектирование</h3>
+
+                <div class="service-hr"></div>
+                <div class="service-description">Бесплатный и дизайн</div>
+            </div>
+        </div>
+    </div>
+    <div class="project">
+        <a href="#" class="project-item project-title">
+            <span class="pr-title">Проекты</span>
+            <span class="pr-count">234</span>
+        </a>
+        <a href="#" class="project-item">
+            <img src="../../../i/test.png">
+
+            <div class="project-description">
+                <span>
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                aasdasdasdaasdasdasd
+                    </span>
+            </div>
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+        <a href="#" class="project-item">
+            <img src="">
+        </a>
+    </div>
+    <div class="news">
+        <a href="#" class="news-item news-title">
+            <span class="nw-title">Октябрь</span>
+            <span class="nw-count">234</span>
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+        <a href="#" class="news-item">
+            <img src="">
+        </a>
+    </div>
+</div>
+<div class="index-sidebar">
+    <a href="#" class="manufacturer">
+        <img src="../../../i/test.png">
+        <img class="manufacturer-logo" src="../../../i/logo.png">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+    <a href="#" class="manufacturer">
+        <img src="">
+    </a>
+</div>
 
