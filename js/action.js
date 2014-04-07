e7ce96dfb8a8f983af4b243eaa2ffe6e52e2fdf0
Алия <vjzkbb@gmail.com>
2014-04-07 17:47:17 +0400

    fix

--- js/action.js
+++ js/action.js
@@ -14,4 +14,17 @@ $(function () {
     $('.dialog-close').click(function () {
         $('.callback-dialog').hide();
     });
+
+    /*
+     * слайдер
+     */
+    if ($('.slider').length > 0)
+        $('.slider').mobilyslider({
+            transition: 'fade',
+            animationSpeed: 800,
+            autoplaySpeed: 3000,
+            autoplay: true,
+            bullets: false,
+            arrowsHide: false
+        });
 });
