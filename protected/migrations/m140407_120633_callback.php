f61116c22073f945e48f9666191f5d5baeea407e
gryaznov M.V <lkdnvc@gmail.com>
2014-04-07 16:34:41 +0400

    action.js

---
+++ protected/migrations/m140407_120633_callback.php
@@ -0,0 +1,22 @@
+<?php
+
+class m140407_120633_callback extends CDbMigration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{callback}}', array(
+            'id' => 'pk',
+            'text' => 'TEXT',
+            'name' => 'VARCHAR(255) NOT NULL',
+            'phone' => 'VARCHAR(255) NOT NULL',
+            'created' => 'datetime NOT NULL',
+        ));
+
+
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{callback}}');
+    }
+}
