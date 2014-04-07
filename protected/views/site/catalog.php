6e7dc0a498b242cb77016f1dc1ac519db0328fd1
Алия <vjzkbb@gmail.com>
2014-04-07 14:31:41 +0400

    add files

---
+++ protected/views/site/catalog.php
@@ -0,0 +1,70 @@
+<?php
+$this->pageTitle = "Penta House - Элитная сантехника и плитка. Продажа. Монтаж. Сервис.";
+?>
+
+<div class="catalog">
+    <div class="catalog">
+        <a href="/site/brand" class="catalog-item catalog-title">
+            <img class="catalog-logo" src="../../../i/logo.png">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="../../../i/test.png">
+
+            <div class="catalog-description">
+                <span>
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                </span>
+            </div>
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item catalog-title">
+            <img class="catalog-logo" src="../../../i/logo.png">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="../../../i/test.png">
+
+            <div class="catalog-description">
+                <span>
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd aasdasdasdaasdasdasd
+                </span>
+            </div>
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+        <a href="#" class="catalog-item">
+            <img src="">
+        </a>
+    </div>
+</div>
