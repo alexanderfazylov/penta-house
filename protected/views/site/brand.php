782e2cea501891a8b36458b071d01d8411f1efc3
Maks Gryaznov <lkdnvc@gmail.com>
2014-06-21 15:43:29 +0400

    bug fix

--- protected/views/site/brand.php
+++ protected/views/site/brand.php
@@ -3,7 +3,7 @@
     <input type="hidden" id="entity_id" value="<?php echo $model->id ?>">
     <?php if (!empty($model->collection) && Helper::issetPhoto($model->collection)): ?>
         <div class="hack-wrapper">
-            <img src="/i/hack_5_3.jpg" class="hack-height" width="100%"/>
+            <img src="/i/hack.jpg" class="hack-height" width="100%"/>
 
             <div class="brand-plugin">
                 <div id="carousel" class="carousel slide" data-ride="carousel">
@@ -16,7 +16,7 @@
                                 $photo = $collection_upload[0];
                                 ?>
                                 <div class="item <?php echo ($index == 0) ? 'active' : '' ?>">
-                                    <img 
+                                    <img
                                     <?php echo Helper::getSrc($photo->upload); ?> />
                                 </div>
                                 <?php ++$index; ?>
